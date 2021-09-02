1f70b0a0efe0592b660c19f0719c6b2c3770e1b1
kenty2442 <mcky874@gmail.com>
2021-09-03 08:54:35 +0900

    first

--- config/firebase.js
+++ config/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import firestore from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDB-M8vgSBtiXIVkGoZ3NS6E2Smg0HXPSE",
-    authDomain: "questionnaire-26be8.firebaseapp.com",
-    projectId: "questionnaire-26be8",
-    storageBucket: "questionnaire-26be8.appspot.com",
-    messagingSenderId: "653184327490",
-    appId: "1:653184327490:web:0ddb1cc59e0805eca1f2db"
+    apiKey: ,
+    authDomain: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: 
 };
 if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig);
